acf69477c667429d4c40985d566e9cfff07f0b0b
siddhesh <21078949+siddheshkand@users.noreply.github.com>
2018-01-30 22:29:12 +0530

    Creates database add words to it with there count Project Completed.

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,31 @@
+import sqlite3 as lite
+
+
 def create_database(database_path: str):
-    # TODO: generate the database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("DROP TABLE IF EXISTS words")
+        ddl = "CREATE TABLE words(word TEXT NOT NULL PRIMARY KEY,usage_count INT DEFAULT 1 NOT NULL);"
+        cur.execute(ddl)
+        ddl = "CREATE UNIQUE INDEX words_word_uindex ON words (word);"
+        cur.execute(ddl)
+    conn.close()
 
 
 def save_words_to_database(database_path: str, word_list: list):
-    # TODO: save database to the database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in word_list:
+            # check if word in there
+            sql = "SELECT count(word) FROM words WHERE word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "UPDATE words SET usage_count= usage_count+1 WHERE word = '" + word + "'"
+            else:
+                sql = "INSERT INTO words(word) VALUES ('" + word + "')"
+            cur.execute(sql)
+    conn.close()
+    print("Database save complete")
